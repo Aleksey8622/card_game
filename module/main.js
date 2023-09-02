7dadbfe9cc3cb239d11648a487251c8da640c8d0
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-09-02 16:18:52 +0300

    Перенес разметку и сделал класс на кнопки выбора сложности

--- module/main.js
+++ module/main.js
@@ -1,7 +1,35 @@
-const conteinerElement = document.querySelector("container");
+const conteinerElement = document.querySelector(".container");
+let gameLavel = ""
+
 
 function getRender() {
 
-    const blokHtml = 
-    
+    let blokHtml = `        <div class="box">
+    <div class="title">Выбери сложность</div>
+    <div class="radio-toolbar">
+        <button class="radio-button">1</button>
+        <button class="radio-button">2</button>
+        <button class="radio-button">3</button>
+    </div>
+    <button class="button-start">Старт</button>
+</div>`
+
+    conteinerElement.innerHTML = blokHtml
+
+    const buttonElements = document.querySelectorAll(".radio-button");
+    const buttonStart = document.querySelector(".buttonStart");
+
+    for (const buttonElement of buttonElements) {
+        buttonElement.addEventListener('click', () => {
+            if (buttonElement.classList.contains("color")) {
+                buttonElement.classList.remove("color")
+            } else {
+                buttonElement.classList.add("color")
+            }
+            console.log("hello");
+        })
+    }
 }
+
+
+getRender()
