50fc66d6fe37bdb226842790330b2be8177da409
Aleksey8622 <121502314+Aleksey8622@users.noreply.github.com>
2023-09-02 19:27:39 +0300

    переделал кнопки на radio

--- module/main.js
+++ module/main.js
@@ -7,24 +7,28 @@ function getRender() {
     let blokHtml = `        <div class="box">
     <div class="title">Выбери сложность</div>
     <div class="radio-toolbar">
-        <button class="radio-button">1</button>
-        <button class="radio-button">2</button>
-        <button class="radio-button">3</button>
+    <input type="radio" id="radio1" name="radios" value="easy" checked>
+    <label for="radio1">1</label>
+    
+    <input type="radio" id="radio2" name="radios" value="medium">
+    <label for="radio2">2</label>
+    
+    <input type="radio" id="radio3" name="radios" value="hard">
+    <label for="radio3">3</label>
     </div>
     <button class="button-start">Старт</button>
 </div>`
 
     conteinerElement.innerHTML = blokHtml
 
-    const buttonElements = document.querySelectorAll(".radio-button");
+    const buttonElements = document.getElementsByName("radios");
     const buttonStart = document.querySelector(".buttonStart");
-
+    console.log(buttonElements);
     for (const buttonElement of buttonElements) {
+        
         buttonElement.addEventListener('click', () => {
-            if (buttonElement.classList.contains("color")) {
-                buttonElement.classList.remove("color")
-            } else {
-                buttonElement.classList.add("color")
+            if (buttonElement.checked) {
+                alert(buttonElement.value)
             }
             console.log("hello");
         })
